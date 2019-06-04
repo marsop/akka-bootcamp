faf3a1eaeccd3fd57238d9ecb872db8240e9e4a2
Alberto Gregorio <alberto.gregorio@phios.li>
2019-06-04 15:41:17 +0200

    finish lesson 1.6

--- src/Unit-1/DoThis/TailActor.cs
+++ src/Unit-1/DoThis/TailActor.cs
@@ -65,15 +65,21 @@ namespace WinTail
 
         private readonly string _filePath;
         private readonly IActorRef _reporterActor;
-        private readonly FileObserver _observer;
-        private readonly Stream _fileStream;
-        private readonly StreamReader _fileStreamReader;
+        private  FileObserver _observer;
+        private  Stream _fileStream;
+        private  StreamReader _fileStreamReader;
 
         public TailActor(IActorRef reporterActor, string filePath)
         {
             _reporterActor = reporterActor;
             _filePath = filePath;
+        }
 
+        /// <summary>
+        /// Initialization logic for actor that will tail changes to a file.
+        /// </summary>
+        protected override void PreStart()
+        {
             // start watching file for changes
             _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
             _observer.Start();
@@ -84,9 +90,22 @@ namespace WinTail
                 FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
             _fileStreamReader = new StreamReader(_fileStream, Encoding.UTF8);
 
-            // read the initial contents of the file and send it to console as first msg
+            // read the initial contents of the file and send it to console as first message
             var text = _fileStreamReader.ReadToEnd();
             Self.Tell(new InitialRead(_filePath, text));
+        }
+
+        /// <summary>
+        /// Cleanup OS handles for <see cref="_fileStreamReader"/> 
+        /// and <see cref="FileObserver"/>.
+        /// </summary>
+        protected override void PostStop()
+        {
+            _observer.Dispose();
+            _observer = null;
+            _fileStreamReader.Close();
+            _fileStreamReader.Dispose();
+            base.PostStop();
         }
 
         protected override void OnReceive(object message)
