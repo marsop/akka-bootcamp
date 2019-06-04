e3ea83d34607c8ddc6fea815544a913fe34fb8e2
Alberto Gregorio <alberto.gregorio@phios.li>
2019-06-04 15:32:10 +0200

    finish lesson 1.5

--- src/Unit-1/DoThis/FileValidationActor.cs
+++ src/Unit-1/DoThis/FileValidationActor.cs
@@ -14,13 +14,10 @@ namespace WinTail
     public class FileValidatorActor : UntypedActor
     {
         private readonly IActorRef _consoleWriterActor;
-        private readonly IActorRef _tailCoordinatorActor;
 
-        public FileValidatorActor(IActorRef consoleWriterActor,
-            IActorRef tailCoordinatorActor)
+        public FileValidatorActor(IActorRef consoleWriterActor)
         {
             _consoleWriterActor = consoleWriterActor;
-            _tailCoordinatorActor = tailCoordinatorActor;
         }
 
         protected override void OnReceive(object message)
@@ -34,41 +31,42 @@ namespace WinTail
                 // tell sender to continue doing its thing (whatever that may be,
                 // this actor doesn't care)
                 Sender.Tell(new Messages.ContinueProcessing());
-                }
-            else
-                {
-                    var valid = IsFileUri(msg);
-                    if (valid)
-                    {
-                        // signal successful input
-                        _consoleWriterActor.Tell(new Messages.InputSuccess(
-                            string.Format("Starting processing for {0}", msg)));
-
-                        // start coordinator
-                        _tailCoordinatorActor.Tell(new TailCoordinatorActor.StartTail(msg,
-                            _consoleWriterActor));
-                    }
-                    else
-                    {
-                        // signal that input was bad
-                        _consoleWriterActor.Tell(new Messages.ValidationError(
-                            string.Format("{0} is not an existing URI on disk.", msg)));
-
-                        // tell sender to continue doing its thing (whatever that
-                        // may be, this actor doesn't care)
-                        Sender.Tell(new Messages.ContinueProcessing());
-                    }
-                }
             }
-
-            /// <summary>
-            /// Checks if file exists at path provided by user.
-            /// </summary>
-            /// <param name="path"></param>
-            /// <returns></returns>
-            private static bool IsFileUri(string path)
+            else
             {
-                return File.Exists(path);
+                var valid = IsFileUri(msg);
+                if (valid)
+                {
+                    // signal successful input
+                    _consoleWriterActor.Tell(new Messages.InputSuccess(
+                        string.Format("Starting processing for {0}", msg)));
+
+                    // start coordinator
+                    Context
+                    .ActorSelection("akka://MyActorSystem/user/" + nameof(TailCoordinatorActor))
+                    .Tell(new TailCoordinatorActor.StartTail(msg, _consoleWriterActor));
+                }
+                else
+                {
+                    // signal that input was bad
+                    _consoleWriterActor.Tell(new Messages.ValidationError(
+                        string.Format("{0} is not an existing URI on disk.", msg)));
+
+                    // tell sender to continue doing its thing (whatever that
+                    // may be, this actor doesn't care)
+                    Sender.Tell(new Messages.ContinueProcessing());
+                }
             }
         }
+
+        /// <summary>
+        /// Checks if file exists at path provided by user.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private static bool IsFileUri(string path)
+        {
+            return File.Exists(path);
+        }
     }
+}
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -18,10 +18,10 @@ namespace WinTail
 
             var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()), nameof(ConsoleWriterActor));
 
-            Props fileValidatorActorProps = Props.Create(() =>new FileValidatorActor(consoleWriterActor, tailCoordinatorActor));
+            Props fileValidatorActorProps = Props.Create(() =>new FileValidatorActor(consoleWriterActor));
             IActorRef validationActor = MyActorSystem.ActorOf(fileValidatorActorProps, "validationActor");
 
-            var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(validationActor)), nameof(ConsoleReaderActor));
+            var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor()), nameof(ConsoleReaderActor));
 
             // tell console reader to begin
             consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
